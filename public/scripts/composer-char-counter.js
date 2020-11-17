823c790b239065239d6a6ce63ee0d79f71de078f
Ramon Ariel Leon Flores <arielleon24@gmail.com>
2020-11-17 22:22:00 +0000

    before finishing tweets

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("#tweet-text").on("keyup",function() {
+    let input = $(this);
+    const form = input.closest("form");
+    const counter = form.find(".counter");
+    let value = 140 - input.val().length
+    counter.html(value); 
+    if (value < 0) {
+      counter.addClass("lessThan0");
+    } else {
+      counter.removeClass("lessThan0");
+    }
+  })
+});
+
